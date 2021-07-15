2174c94490cbdb156a0fa8feb8bcd20eddda2d2c
Solidshots <seanbrent5@gmail.com>
2021-07-15 08:14:04 -0400

    cart reducers and tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,5 +1,16 @@
-import { UPDATE_PRODUCTS, UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from "./actions";
 import {useReducer} from 'react';
+import { 
+    UPDATE_PRODUCTS, 
+    UPDATE_CATEGORIES, 
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+} from "./actions";
+
 
 export const reducer = (state, action) => {
     switch(action.type){
@@ -21,6 +32,56 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 currentCategory: action.currentCategory
+            };
+
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+        
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, ...action.products]
+            };
+        
+        case REMOVE_FROM_CART:
+            let newCart = state.cart.filter(item => item._id !== action._id)
+
+            return {
+                ...state,
+                cartOpen: newCart > 0,
+                cart: newCart
+            };
+
+        case UPDATE_CART_QUANTITY: 
+            let updatedCart = state.cart.map(item => {
+                if(item._id === action._id){
+                    item.productQuantity = action.quantity;
+                }
+                return item;
+            });
+
+            return {
+                ...state,
+                cartOpen: true,
+                cart: updatedCart
+            };
+
+        case CLEAR_CART: 
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: action.cartOpen
             }
 
         //if we are not updating anything then we do not update state at all
